a2b93408585152bd237211bc086c24318e6d7b9f
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-11-07 23:05:42 +0100

    update

--- main.js
+++ main.js
@@ -4,27 +4,33 @@ const btn_clear = document.querySelector('.btn-clear')
 const addedItems = new Set()
 item_form.addEventListener('submit',(e)=>{
   e.preventDefault()
-  if(!form_input.value){
+  const inputValue = form_input.value
+  if(!inputValue){
     alert('please enter a list item')
     return
   }
-  if(addedItems.has(form_input.value)){
+  if(addedItems.has(inputValue)){
     alert('this item was already added')
     return
   }
   const item_list = document.getElementById('item-list')
   const remove_item = document.createElement('button')
-  remove_item.classList.add('remove-item', 'btn-link', 'text-red')
+  remove_item.classList.add('remove-item')
   remove_item.textContent = 'x'
   const li_el = document.createElement('li')
-  li_el.textContent = form_input.value
+  li_el.textContent = inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
   addedItems.add(li_el.textContent)
   item_list.append(li_el)
   li_el.append(remove_item)
   form_input.value = ""
   btn_clear.style.display = 'block'
+  // form_input.addEventListener('keyup',()=>{
+  //   const item_list = document.querySelectorAll('.items.children')
+  //   console.log(item_list)
+  // })
   remove_item.addEventListener('click',()=>{
     li_el.remove()
+    addedItems.clear()
   })
   btn_clear.addEventListener('click',()=>{
      li_el.remove()
